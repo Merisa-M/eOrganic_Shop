45b1f62af0ea384f10cfc039f6132334d8cdc4fe
Merisa-M <merisa.mulac@gmail.com>
2022-03-23 20:20:37 +0100

    proizvodi controller

--- _eOrganicShop/eOrganicShop/Controllers/ProizvodiController.cs
+++ _eOrganicShop/eOrganicShop/Controllers/ProizvodiController.cs
@@ -1,5 +1,6 @@
 ﻿using eOrganicShop.Model.Request;
 using eOrganicShop.Service;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -19,26 +20,31 @@ namespace eOrganicShop.Controllers
             _service = service;
         }
         [HttpGet]
+        [Authorize]
         public async Task<List<Model.Proizvodi>> Get([FromQuery] ProizvodiSearchRequest search)
         {
             return await _service.Get(search);
         }
         [HttpGet("{ID}")]
+        [Authorize]
         public async Task<Model.Proizvodi> GetById(int ID)
         {
             return await _service.GetById(ID);
         }
         [HttpPost]
+        [Authorize(Roles = "Admin, Kupac")]
         public async Task<Model.Proizvodi> Insert(ProizvodiUpsertRequest request)
         {
             return await _service.Insert(request);
         }
         [HttpPut("{ID}")]
+        [Authorize]
         public async Task<Model.Proizvodi> Update(int ID, ProizvodiUpsertRequest request)
         {
             return await _service.Update(ID, request);
         }
         [HttpDelete("{ID}")]
+        [Authorize(Roles = "Admin")]
         public async Task<bool> Delete(int ID)
         {
             return await _service.Delete(ID);
